70f77efeddf9dce89ad5a6f5e58e482dd7271805
Akoruu.Dev <akoruu.dev@gmail.com>
2022-11-10 00:04:02 -0300

    FEAT [Routes]: Create methods for request on routes from App

--- src/app.js
+++ src/app.js
@@ -10,6 +10,37 @@ app.use(express.json());
 const mongoClient = new MongoClient(process.env.MONGO_URI);
 let db;
 
-app.listen(4000, () => {
+// connect
+try {
+    await mongoClient.connect();
+    db = mongoClient.db('chatuol');
+} catch (err) {
+    console.log(err);
+}
+
+// routes
+app.post('/participants', (req, res) => {
+
+});
+
+app.get('/participants', (req, res) => {
+
+});
+
+app.post('/messages', (req, res) => {
+
+});
+
+app.get('/messages', (req, res) => {
+
+});
+
+app.post('/status', (req, res) => {
+
+});
+
+
+
+app.listen(5000, () => {
     console.log('Server running');
 });
