d0c45136264ba93f8fbbb82d0d2a395e01198716
Akoruu.Dev <akoruu.dev@gmail.com>
2022-11-10 00:38:59 -0300

    CHORE [User]: Start create a function for register new user on App

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import { MongoClient } from "mongodb";
+import Joi from "joi";
 
 const app = express();
 
@@ -20,7 +21,36 @@ try {
 
 // routes
 app.post('/participants', (req, res) => {
+    const name = req.body;
 
+    const newUser = Joi.object({
+        name: Joi
+                .string()
+                .min(1)
+                .required()
+    }).options({ abortEarly: false });
+  
+    const response = newUser.validate(name);
+
+    if (response.error) {
+        res.status(422).send(response.error.details);
+        return;
+    }
+
+    thereIsName = db.collection('users').find(user => {
+        if (user.name === newUser.name) {
+            return true;
+        }
+        return false;
+    })
+
+    if (thereIsName) {
+        res.status(409).send('Este usuário já existe');
+        return;
+    }
+
+    db.collection('users').insert(newUser);
+    res.status(201).send(`Cadastro concluido. Bem vind@ ${newUser.name}`);
 });
 
 app.get('/participants', (req, res) => {
